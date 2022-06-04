a0478fbadc85978e47a4f0580d9bf1ab3689bca1
alansshots <alengospodinov@gmail.com>
2022-06-05 02:06:51 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <template>
   <div id="app">
+    <!-- Navbar -->
+    <div class="navbar-container">
+      <div>
+        <h2>Colorblock</h2>
+      </div>
+
+    <div class="main-nav"> 
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Начало</router-link>
+      <router-link to="/about">Информация</router-link>
+      <router-link to="/projects">Проекти</router-link>
     </nav>
+    </div>
+
+    <div>
+      <button>Контакти</button>
+    </div>
+
+    </div>
+
+    <!-- Load content -->
     <router-view/>
+
+    <!-- Footer -->
   </div>
 </template>
 
 <style>
-#app {
+  @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap');
+
+  * {
+    font-family: 'Comfortaa', cursive;
+  }
+
+  .navbar-container {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .main-nav nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-content: center;
+  }
+
+  .main-nav nav a {
+    padding: 0rem 1rem;
+  }
+
+
+
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -28,5 +74,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Banner -->
+    <div class="banner">
+      <div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur elit!</p>
+        <button>Повече Информация</button>
+      </div>
+    </div>
+
+    <!-- Info section -->
+
+    <div class="info-section">
+      <!-- Info box -->
+      <div>
+        <p>about</p>
+        <div>
+          <h3>Lorem, ipsum dolor.</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, omnis aperiam obcaecati temporibus quidem iste neque aliquid magnam repellat delectus.</p>
+          <button>Learn more</button>
+        </div>
+      <div>
+
+        </div>
+      </div>
+
+      <!-- Img -->
+      <div>
+       <img src="" alt=""> 
+      </div>  
+
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    
+  },
 }
 </script>
+
+<style scoped>
+
+.banner {
+  height: 60%;
+  background: #ccc;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
