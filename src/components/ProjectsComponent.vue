5c6f00fb1132dd1683fbfbad155fcde0b74c0bca
alansshots <alengospodinov@gmail.com>
2022-08-21 13:09:48 +0300

    flicker projects

---
+++ src/components/ProjectsComponent.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse mollitia asperiores expedita quis rem totam minus commodi, nam eligendi beatae?</h5>
+    <carousel-3d>
+        <slide :index="0" class="slide">
+            <h2>Butilkavino</h2>
+            <img src="../assets/ButilkavinoText.jpg" alt="">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis libero harum, reprehenderit pariatur veritatis ducimus!</p>
+            <button class="info-btn">Виж още</button>
+        </slide>
+        <slide :index="1" class="slide">
+            <h2>Vremednes</h2>
+            <img src="" alt=""></img>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis libero harum, reprehenderit pariatur veritatis ducimus!</p>
+            <button class="info-btn">Виж още</button>
+        </slide>
+        <slide :index="2" class="slide">
+            <h2>Cryptocoin</h2>
+            <img src="" alt=""></img>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis libero harum, reprehenderit pariatur veritatis ducimus!</p>
+            <button class="info-btn">Виж още</button>
+        </slide>
+        <slide :index="3" class="slide">
+            <h2>Wine Project</h2>
+            <img src="" alt=""></img>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis libero harum, reprehenderit pariatur veritatis ducimus!</p>
+            <button class="info-btn">Виж още</button>
+        </slide>
+        <slide :index="4" class="slide">
+            <h2>Axis</h2>
+            <img src="" alt=""></img>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis libero harum, reprehenderit pariatur veritatis ducimus!</p>
+            <button class="info-btn">Виж още</button>
+        </slide>
+    </carousel-3d>
+  </div>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+export default {
+   name: 'ProjectsComponent',
+   components: {
+    Carousel3d,
+    Slide
+    },
+}
+</script>
+
+<style scoped>
+  .container {
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+  }
+
+  .slide {
+      background: #fff;
+      border: 1px solid #000;
+      padding: 10px;
+      text-align: center;
+  }
+
+  .slide img {
+    width: 200px;
+  }
+
+  .info-btn {
+    margin-top: 0.5rem;
+    cursor: pointer;
+    padding: 0.5rem 2rem;
+    color: #fff;
+    background: #000;
+    border: 1px solid #fff;
+    transition:  0.2s;
+  }
+
+  .info-btn:hover {
+    padding: 0.5rem 2rem;
+    color: #000;
+    background: #fff;
+    border: 1px solid #000;
+    transition:  0.2s;
+  }
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -8,66 +8,19 @@
     </div>
 
   <!-- Projects -->
-  <main>
-    <div class="short">
-      <h3>Cryptocoin</h3>
-      <p>Уеб сайт за проследяване на цените на криптоактиви изработен чрез Vue.js и TailwindCSS. Неговата мисия е да предостави на потребителите висококачествена и точна информация относно криптоактиви.</p>
-      <a href="http://wine-project.net/" target="_blank">
-          <button class="info-btn" >Виж повече</button>
-      </a>
-    </div>
-    <div class="tall">
-      <img src="" alt="site-img">
-    </div>
-    <div class="short">
-      <h3>Butilkavino.com</h3>
-      <p>Онлайн магазин изработен чрез Wordpress. Butilkavino.com предлага, богат асортимент от вина и други продукти свързани с виното от различни винарски региони на България.</p>
-      <a href="http://butilkavino.com/" target="_blank">
-          <button class="info-btn" >Виж повече</button>
-      </a>
-    </div>
-    <div class="tall">
-      <h3>Wine Project</h3>
-      <p>Информационен уеб сайт изработен чрез HTML, CSS и JavaScript. ВАЙН ПРОДЖЕКТ ЕООД извършва проектиране, внедряване и поддръжка на съвременни ИТ решения, базирани на широка гама от продукти и решения на утвърдени световни производители.</p>
-      <a href="http://wine-project.net/" target="_blank">
-          <button class="info-btn" >Виж повече</button>
-      </a>
-    </div>
-    <div class="tall">
-      <img src="" alt="site-img">
-    </div>
-    <div class="short">
-      <h3>Axis</h3>
-      <p>Форум изработен чрез Laravel, JavaScript и TailwindCSS. В сайта може да бъде намерена информация за това как се използва 3D-принтер, както и обща информация за 3D принтирането.</p>
-      <a href="http://3dprint.webimpression.net/home" target="_blank">
-          <button class="info-btn" >Виж повече</button>
-      </a>
-    </div>
-    <!-- <div class="short">
-      <h3>A note about style</h3>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, reprehenderit.</p>
-      <a href="">Read more</a>
-    </div> -->
-    <!-- <div class="tall">
-      <img src="img/1.png" alt="model">
-    </div> -->
-    <!-- <div class="short">
-      <h3>A note about style</h3>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, reprehenderit.</p>
-      <a href="">Read more</a>
-    </div> -->
-    <!-- <div class="short">
-      <h3>A note about style</h3>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, reprehenderit.</p>
-      <a href="">Read more</a>
-    </div> -->
-  </main>
+  <ProjectsComponent/>
+  
 </div>
 </template>
 
 <script>
+import ProjectsComponent from '../components/ProjectsComponent.vue'
+
 export default {
- name:'ProjectsView',
+   name:'ProjectsView',
+   components: {
+    ProjectsComponent,
+  }
 }
 </script>
 
@@ -96,83 +49,9 @@ export default {
   align-items: center;
 }
 
-
   /* Projects */
-  main {
-    max-width: 1200px;
-    margin: 60px auto;
-    padding: 20px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-auto-rows: 250px;
-    gap: 30px;
-  }
-
-  main div {
-    overflow: hidden;
-    padding: 1rem;
-    background: #fff;
-    border: 1px solid #000;
-    box-shadow: 3px 3px 5px rgba(0,0,0,0.05);
-  }
-
-  div h3 {
-    margin: 0.5rem 0;
-  }
-
-  main img {
-    min-width: 100%;
-    /* height: 100%; */
-  }
-
-  main .short {
-    grid-row: span 1;
-  }
-
-  main .tall {
-    grid-row: span 2;
- }
-
-  .info-btn {
-    cursor: pointer;
-    padding: 0.5rem 0.5rem;
-    color: #fff;
-    background: #000;
-    border: 1px solid #fff;
-    transition:  0.2s;
-  }
-
-  .info-btn:hover {
-    padding: 0.5rem 0.5rem;
-    color: #000;
-    background: #fff;
-    border: 1px solid #000;
-    transition:  0.2s;
-  }
 
   /* Responsive */
-
-  @media screen and (max-width: 960px) {
-    main {
-      grid-template-columns: 1fr 1fr;
-    }
-  }
-
-  @media screen and (max-width: 620px) {
-    main {
-      grid-template-columns: 1fr;
-      max-width: 400px;
-      margin-top: 20px;
-    }
-    nav {
-      grid-template-columns: repeat(4, 1fr);
-    }
-    nav h1{
-      grid-column: 1 / span 4;
-      grid-row: 1;
-    }  
-  }
-
   @media only screen and (max-width: 600px) {
 
     .banner-text-container h1 {
